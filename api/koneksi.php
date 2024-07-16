70cf2658dfb30fb09aaca7bb28d1dacc44385800
mkurniarmadhan <mkurniarmadhan@gmail.com>
2024-07-16 21:13:57 +0700

    pwa-masdi

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,11 +1,18 @@
 <?php
 class KoneksiDatabase
 {
+
     private $host = 'localhost';  // Sesuaikan dengan host database Anda
-    private $username = 'root';   // Sesuaikan dengan username database Anda
-    private $password = '';       // Sesuaikan dengan password database Anda
-    private $database = 'db_pwa'; // Sesuaikan dengan nama database Anda
+    private $username = 'u694229934_masdi';   // Sesuaikan dengan username database Anda
+    private $password = 'Masdi321#';       // Sesuaikan dengan password database Anda
+    private $database = 'u694229934_masdi'; // Sesuaikan dengan nama database Anda
     protected $koneksi;
+
+    // private $host = 'localhost';
+    // private $username = 'root';
+    // private $password = '';
+    // private $database = 'db_pwa';
+    // protected $koneksi;
 
     // Konstruktor untuk koneksi database
     public function __construct()
